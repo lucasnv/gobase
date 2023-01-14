71d125cc27b031cae29a3504563955e7ccc329c3
Lucas Vazquez <lucasnvazquez@gmail.com>
2023-01-13 21:10:56 -0300

    [MASTER] Install file hotfix

--- bin/install.sh
+++ bin/install.sh
@@ -3,24 +3,32 @@ EXCLUDE_DIR=
 # Colors
 Color_Off='\033[0m'       # Text Reset
 Yellow='\033[0;33m'       # Yellow
+Green='\033[0;32m'        # Green
 
 printf "\n"
-printf "${Yellow}> Installing proyect ... ${Color_Off} \n"
+printf "${Yellow}> Installing, wait a moment please... ${Color_Off} \n"
 printf "\n"
 
 
-# grep -rl --exclude-dir=.git --exclude-dir=bin --exclude-dir=.github --exclude-dir=cmd --exclude-dir=config --exclude-dir=db --exclude-dir=scripts "[MODULE_URL]" .
-
+# set the module URL
 grep -rl --exclude-dir=.git \
          --exclude-dir=bin \
          --exclude-dir=.github \
-         --exclude-dir=cmd \
          --exclude-dir=config \
          --exclude-dir=db \
          --exclude-dir=scripts \
          '<MODULE_URL_REPLACE>' . | xargs sed -i "s+<MODULE_URL_REPLACE>+$1+g"
 
-# find . -type f -exec sed -i 's+[MODULE_URL]+PRUEBAAAAAAAA+g' {} +
+# set the container name
+grep -rl --exclude-dir=.git \
+         --exclude-dir=bin \
+         --exclude-dir=.github \
+         --exclude-dir=config \
+         --exclude-dir=db \
+         --exclude-dir=scripts \
+         '<CONTAINER_NAME_REPLACE>' . | xargs sed -i "s+<CONTAINER_NAME_REPLACE>+$2+g"         
 
-#grep -rl '[MODULE_URL]' bin/test/ | xargs sed -i "s+[MODULE_URL]+$1+g"
-# grep -rl [CONTAINER_NAME] test/ | xargs sed -i "s+[CONTAINER_NAME]+$2+g"
+
+printf "\n"
+printf "${Green}> Installation complete. ${Color_Off} \n"
+printf "\n"
